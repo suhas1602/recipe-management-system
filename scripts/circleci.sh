d0391fe04c6545b8593c5589d210e15735e54342
Suhas Pasricha <pasrichasuhas@gmail.com>
2019-11-06 01:27:54 -0500

    added region to script

--- scripts/circleci.sh
+++ scripts/circleci.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 pwd
 ls -a
+echo $AWS_REGION
 
 fileName="$(ls | grep zip)"
 echo "${fileName}"
 aws s3 cp "${fileName}" "s3://codedeploy.suhaspasricha.com/${fileName}"
 
-ETag="$(aws s3api head-object --bucket codedeploy.suhaspasricha.com --key ${fileName} --query ETag --output text)"
+ETag="$(aws s3api head-object --bucket codedeploy.suhaspasricha.com --key ${fileName} --query ETag --output text --region ${AWS_REGION})"
 
 ETag="${ETag%\"}"
 ETag="${ETag#\"}"
 echo $ETag
 
-deploymentId="$(aws deploy create-deployment --application-name csye6225-webapp --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name csye6225-webapp-deployment --description "Webapp deployment" --s3-location bucket=codedeploy.suhaspasricha.com,bundleType=zip,eTag=${ETag},key=${fileName} | jq '.deploymentId')"
+deploymentId="$(aws deploy create-deployment --application-name csye6225-webapp --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name csye6225-webapp-deployment --description "Webapp deployment" --s3-location bucket=codedeploy.suhaspasricha.com,bundleType=zip,eTag=${ETag},key=${fileName} --region ${AWS_REGION} | jq '.deploymentId')"
 
 deploymentId="${deploymentId%\"}"
 deploymentId="${deploymentId#\"}"
