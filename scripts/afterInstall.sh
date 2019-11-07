f5315ba6a44dd5f0a7fc9e8fc2e1e68d1cb22c9b
Suhas Pasricha <pasrichasuhas@gmail.com>
2019-11-06 22:49:31 -0500

    run with root privilege afterInstall.sh

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,5 +2,4 @@
 cd "/home/centos/webserver"
 npm install
 pwd
-node webapp/index.js > /dev/null 2> /dev/null < /dev/null &
-lsof -i:3000
+sudo node webapp/index.js > /dev/null 2> /dev/null < /dev/null &
