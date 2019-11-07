66c6bc26f5128f7505401fc9d3e8be66b899caeb
Suhas Pasricha <pasrichasuhas@gmail.com>
2019-11-06 22:59:37 -0500

    added cloudwatch config

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,4 +2,9 @@
 cd "/home/centos/webserver"
 npm install
 pwd
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/home/centos/webserver/cloudwatch-config.json \
+    -s
 sudo node webapp/index.js > /dev/null 2> /dev/null < /dev/null &
