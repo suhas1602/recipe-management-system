59795096dadd2c5ef5183a7d62e10801003e43a2
Suhas Pasricha <pasrichasuhas@gmail.com>
2019-11-07 10:36:31 -0500

    load env variables in codedeploy script

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -8,13 +8,15 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -c file:/home/centos/webserver/cloudwatch-config.json \
     -s
 
-if [! -f "/home/centos/webserver/csye6225.log" ]
+if [ ! -f "/home/centos/webserver/csye6225.log" ]
 then
+    echo "Creating log file"
     touch "/home/centos/webserver/csye6225.log"
 fi
 
 sudo chown centos "/home/centos/webserver/csye6225.log"
 sudo chmod 664 csye6225.log   
+source /etc/environment
 echo $DB_USER
 echo $DB_HOST_NAME 
 pm2 start "/home/centos/webserver/webapp/index.js" --name="csye6225-webapp"
